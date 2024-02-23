fb1047b440bb5c3a97e6005b8127f34b4d16bb2d
RinaKogteva <ledbina@gmail.com>
2024-02-23 23:32:50 +0300

    Создаем окно менюбара

--- Notes.py
+++ Notes.py
@@ -5,3 +5,17 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile, asksaveasfile
 
+#Создаем окно менюбара
+root = Tk()
+root.title("ЗАМЕТКИ")
+root.geometry("400x600")
+text = Text(root,width=400, height=600)
+text.pack()
+ont = StringVar()
+
+frame_ent = Frame(root, bg="grey")
+frame_ent.place(relheight=0.05)
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label='Файл', menu = file_menu)
+
